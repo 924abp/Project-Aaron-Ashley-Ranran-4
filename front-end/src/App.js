87ae5a0478b461bc8f7ba8f5520b51d2d43bdfd4
ZHATC6 <129005247+ZHATC6@users.noreply.github.com>
2023-08-12 21:26:28 -0400

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './Login';
@@ -16,23 +15,4 @@ function App() {
   );
 }
 
-=======
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login';
-import Register from './Register';
-
-function App() {
-
-  return (
-      <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Login />}></Route>
-            <Route path='/signup' element={<Register />}></Route>
-          </Routes>
-      </BrowserRouter>
-  );
-}
-
->>>>>>> 64b6d1d640e2dd8f8de8d73c9d83b2dea67bf62a
 export default App;
