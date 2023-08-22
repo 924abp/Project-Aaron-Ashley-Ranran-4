34de117a218125ff82373f04b22dd20e38327699
Aaron Peterson <924abp@gmail.com>
2023-08-21 21:56:02 -0600

    change bakend

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const recipeRoutes = require("./routes/recipesRoutes")
+const authorRoutes = require("./routes/authorsRoutes")
 
 const app = express()
-const port = 3000
+const port = 3001
+app.use(cors())
 app.use(express.json())
 
 app.use("/recipe", recipeRoutes)
+app.use("/author", authorRoutes)
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
